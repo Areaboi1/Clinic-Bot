201f39ca39498321278ad924948515402d81cb64
Areaboi1 <aryamanpal40@gmail.com>
2023-01-02 19:58:06 +0800

    h

--- Online Clinic/website/static/index.js
+++ Online Clinic/website/static/index.js
@@ -1,11 +1,3 @@
-function deleteNote(noteId) {
-	fetch("/delete-note", {
-		method: "POST",
-		body: JSON.stringify({ noteId: noteId }),
-	}).then((_res) => {
-		window.location.href = "/";
-	});
-}
 function deletebook(bookId) {
 	fetch("/delete-book", {
 		method: "POST",
--- Online Clinic/website/views.py
+++ Online Clinic/website/views.py
@@ -16,28 +16,8 @@ views = Blueprint('views',__name__)
 @views.route('/', methods=['GET','POST'])
 @login_required
 def home():
-    if request.method == "POST":
-        note = request.form.get('note')
-
-        if len(note) < 1:
-            flash('Note is too short', category="error")
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note was created', category="success")
+    
     return render_template("home.html", user=current_user)
-
-@views.route('/delete-note', methods=["POST"])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-    return jsonify({})
 
 @views.route('/book', methods=['GET','POST'])
 @login_required
